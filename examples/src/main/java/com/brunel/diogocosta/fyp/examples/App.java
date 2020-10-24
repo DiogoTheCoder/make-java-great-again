b16fb79128857faddf4a4d51c57c410d8df3e1b7
Diogo Costa <hello@diogothecoder.com>
2020-10-24 20:24:42 +0100

    Created .gitignore for Java examples folder
    Deleted .gitignore files
    
    Added Java examples of concat and summing

--- examples/src/main/java/com/brunel/diogocosta/fyp/examples/App.java
+++ examples/src/main/java/com/brunel/diogocosta/fyp/examples/App.java
@@ -1,12 +1,16 @@
 package com.brunel.diogocosta.fyp.examples;
 
+import static java.lang.String.valueOf;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class App {
     private static Logger logger;
-    private static final Integer SEQUENCE_SIZE = 500000;
+    private static final Integer SEQUENCE_SIZE = 10000;
 
     public static void main(String[] args) {
         logger = Logger.getGlobal();
@@ -14,6 +18,8 @@ public class App {
         logger.fine("Hello World!");
 
         ArrayList<Integer> sequence = generateSequence();
+
+        logger.info("--- ITERATING ARRAYLIST ---");
         timeAndExecuteFunction(() -> forLoopIterate(sequence), "forLoopIterate");
         timeAndExecuteFunction(() -> forEachIterate(sequence), "forEachIterate");
         timeAndExecuteFunction(() -> whileIterate(sequence), "whileIterate");
@@ -21,18 +27,28 @@ public class App {
         timeAndExecuteFunction(() -> whileListIterator(sequence), "whileListIterator");
         timeAndExecuteFunction(() -> functionalForEachIterate(sequence), "functionalForEachIterate");
         timeAndExecuteFunction(() -> functionalStreamForEachIterate(sequence), "functionalStreamForEachIterate");
+
+        logger.info("--- CONCATENATING ARRAYLIST ELEMENTS ---");
+        timeAndExecuteFunction(() -> forEachIterateConcat(sequence), "forEachIterateConcat");
+        timeAndExecuteFunction(() -> functionalForEachIterateConcat(sequence), "functionalForEachIterateConcat");
+        timeAndExecuteFunction(() -> functionalStreamCollectConcat(sequence), "functionalStreamCollectConcat");
+
+        logger.info("--- SUMMING ARRAYLIST ELEMENTS ---");
+        timeAndExecuteFunction(() -> forEachIterateSum(sequence), "forEachIterateSum");
+        timeAndExecuteFunction(() -> functionalForEachIterateSum(sequence), "functionalForEachIterateSum");
+        timeAndExecuteFunction(() -> functionalStreamCollectSum(sequence), "functionalStreamCollectSum");
     }
 
     private static void forLoopIterate(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
-            String number = String.valueOf(list.get(i));
+            String number = valueOf(list.get(i));
             logger.finest(number);
         }
     }
 
     private static void forEachIterate(ArrayList<Integer> list) {
         for (Integer integer : list) {
-            String number = String.valueOf(integer);
+            String number = valueOf(integer);
             logger.finest(number);
         }
     }
@@ -40,7 +56,7 @@ public class App {
     private static void whileIterator(ArrayList<Integer> list) {
         Iterator<Integer> listIterator = list.iterator();
         while (listIterator.hasNext()) {
-            String number = String.valueOf(listIterator.next());
+            String number = valueOf(listIterator.next());
             logger.finest(number);
         }
     }
@@ -48,7 +64,7 @@ public class App {
     private static void whileListIterator(ArrayList<Integer> list) {
         int i = 0;
         while (i < list.size()) {
-            String number = String.valueOf(list.get(i));
+            String number = valueOf(list.get(i));
             logger.finest(number);
             i++;
         }
@@ -56,23 +72,67 @@ public class App {
 
     private static void whileIterate(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
-            String number = String.valueOf(list.get(i));
+            String number = valueOf(list.get(i));
             logger.finest(number);
         }
     }
 
     private static void functionalForEachIterate(ArrayList<Integer> list) {
         list.forEach(integer -> {
-            String number = String.valueOf(integer);
+            String number = valueOf(integer);
             logger.finest(number);
         });
     }
 
     private static void functionalStreamForEachIterate(ArrayList<Integer> list) {
         list.stream().forEach(integer -> {
-            String number = String.valueOf(integer);
+            String number = valueOf(integer);
             logger.finest(number);
         });
+    }
+
+    private static void forEachIterateConcat(ArrayList<Integer> list) {
+        // You can also use a StringBuilder object
+        String output = "";
+        for (Integer integer : list) {
+            output += ", " + valueOf(integer);
+        }
+
+        logger.finest(output);
+    }
+
+    private static void functionalForEachIterateConcat(ArrayList<Integer> list) {
+        StringBuilder output = new StringBuilder();
+        list.forEach(integer -> output.append(", " + valueOf(integer)));
+
+        logger.finest(output.toString());
+    }
+
+    private static void functionalStreamCollectConcat(ArrayList<Integer> list) {
+        String output = list.stream().map(Object::toString).collect(Collectors.joining(", "));
+        logger.finest(output);
+    }
+
+    private static void forEachIterateSum(ArrayList<Integer> list) {
+        // You can also use a StringBuilder object
+        int sum = 0;
+        for (Integer integer : list) {
+            sum += integer;
+        }
+
+        logger.finest(valueOf(sum));
+    }
+
+    private static void functionalForEachIterateSum(ArrayList<Integer> list) {
+        AtomicInteger sum = new AtomicInteger();
+        list.forEach(integer -> sum.getAndAdd(integer));
+
+        logger.finest(valueOf(sum));
+    }
+
+    private static void functionalStreamCollectSum(ArrayList<Integer> list) {
+        Integer sum = list.stream().collect(Collectors.summingInt(Integer::intValue));
+        logger.finest(valueOf(sum));
     }
 
     private static ArrayList<Integer> generateSequence() {
