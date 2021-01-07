57ac46081872fa00e74719db445bbdbf857a8f8f
Diogo Costa <hello@diogothecoder.com>
2021-01-07 16:54:05 +0000

    FYP-4: generate and replace with generic for each stream lambda

--- examples/src/main/java/com/brunel/diogocosta/fyp/examples/Refactor.java
+++ examples/src/main/java/com/brunel/diogocosta/fyp/examples/Refactor.java
@@ -1,12 +1,18 @@
 package com.brunel.diogocosta.fyp.examples;
 
+import java.util.Arrays;
+
 public class Refactor {
 
   public static void main(String[] args) {
     String[] name = { "Diogo", "Costa" };
 
-    for (int x = 0; x < name.length; x++) {
-      System.out.println(name[x]);
+    Arrays.stream(name).forEach(string -> {
+      System.out.println(string);
+    });
+
+    for (String string : name) {
+      System.out.println(string);
     }
   }
 }
