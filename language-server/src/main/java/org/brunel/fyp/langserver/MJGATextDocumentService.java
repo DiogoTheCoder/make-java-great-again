e70efd84a9bcbbb992d6b7ee26c2d0fc1e573e5d
Diogo Costa <hello@diogothecoder.com>
2021-02-10 13:16:20 +0000

    FYP-25: make `variableDeclarationExprs` non-static

--- language-server/src/main/java/org/brunel/fyp/langserver/MJGATextDocumentService.java
+++ language-server/src/main/java/org/brunel/fyp/langserver/MJGATextDocumentService.java
@@ -1,14 +1,82 @@
 package org.brunel.fyp.langserver;
 
-import org.eclipse.lsp4j.*;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.stmt.ForEachStmt;
+import com.github.javaparser.ast.stmt.ForStmt;
+import com.github.javaparser.printer.PrettyPrinterConfiguration;
+import org.brunel.fyp.langserver.refactorings.ForEachRefactoringPattern;
+import org.brunel.fyp.langserver.refactorings.ForLoopRefactoringPattern;
+import org.eclipse.lsp4j.CodeActionParams;
+import org.eclipse.lsp4j.CodeLens;
+import org.eclipse.lsp4j.CodeLensParams;
+import org.eclipse.lsp4j.Command;
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.DocumentFormattingParams;
+import org.eclipse.lsp4j.DocumentHighlight;
+import org.eclipse.lsp4j.DocumentOnTypeFormattingParams;
+import org.eclipse.lsp4j.DocumentRangeFormattingParams;
+import org.eclipse.lsp4j.DocumentSymbolParams;
+import org.eclipse.lsp4j.Hover;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.ReferenceParams;
+import org.eclipse.lsp4j.RenameParams;
+import org.eclipse.lsp4j.SignatureHelp;
+import org.eclipse.lsp4j.SymbolInformation;
+import org.eclipse.lsp4j.TextDocumentPositionParams;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.WorkspaceEdit;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.TextDocumentService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
 public class MJGATextDocumentService implements TextDocumentService {
+    private CompilationUnit compilationUnit;
+    private List<VariableDeclarator> variableDeclarationExprs;
+
+    public String parseFile(String filePath) {
+        try {
+            ParserConfiguration parserConfig = new ParserConfiguration();
+            parserConfig.setIgnoreAnnotationsWhenAttributingComments(true);
+            parserConfig.setDoNotAssignCommentsPrecedingEmptyLines(true);
+            StaticJavaParser.setConfiguration(parserConfig);
+            compilationUnit = StaticJavaParser.parse(new FileInputStream(filePath));
+            variableDeclarationExprs = compilationUnit.findAll(VariableDeclarator.class);
+            compilationUnit.findAll(ForStmt.class)
+                    .forEach(forStmt -> {
+                        compilationUnit = new ForLoopRefactoringPattern().refactor(forStmt, compilationUnit);
+                    });
+
+            compilationUnit.findAll(ForEachStmt.class)
+                    .forEach(forEachStmt -> {
+                        compilationUnit = new ForEachRefactoringPattern().refactor(forEachStmt, compilationUnit);
+                    });
+
+            return compilationUnit.toString(new PrettyPrinterConfiguration().setOrderImports(true));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    public List<VariableDeclarator> getVariableDeclarationExprs() {
+        return this.variableDeclarationExprs;
+    }
+
     @Override
     public CompletableFuture<Either<List<CompletionItem>, CompletionList>> completion(CompletionParams completionParams) {
         return null;
@@ -85,21 +153,19 @@ public class MJGATextDocumentService implements TextDocumentService {
     }
 
     @Override
-    public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
-
-    }
+    public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) { }
 
     @Override
     public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
+        File file = new File(didChangeTextDocumentParams.getTextDocument().getUri());
+
+        String filePath = file.getPath().replaceAll("\"", "");
+        Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info("Parsing Java code from file: " + filePath);
     }
 
     @Override
-    public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
-
-    }
+    public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) { }
 
     @Override
-    public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
-
-    }
+    public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) { }
 }
--- language-server/src/main/java/org/brunel/fyp/langserver/MJGAWorkspaceService.java
+++ language-server/src/main/java/org/brunel/fyp/langserver/MJGAWorkspaceService.java
@@ -1,35 +1,20 @@
 package org.brunel.fyp.langserver;
 
-import org.brunel.fyp.langserver.refactorings.ForEachRefactoringPattern;
-import org.brunel.fyp.langserver.refactorings.ForLoopRefactoringPattern;
 import org.eclipse.lsp4j.DidChangeConfigurationParams;
 import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
 import org.eclipse.lsp4j.ExecuteCommandParams;
 import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.lsp4j.WorkspaceSymbolParams;
 import org.eclipse.lsp4j.services.WorkspaceService;
+import org.json.JSONObject;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
-import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.stmt.ForEachStmt;
-import com.github.javaparser.ast.stmt.ForStmt;
-import com.github.javaparser.printer.PrettyPrinterConfiguration;
-import org.json.JSONObject;
-
 public class MJGAWorkspaceService implements WorkspaceService {
     private JSONObject configurationSettings;
-
-    public static CompilationUnit compilationUnit;
-    public static List<VariableDeclarator> variableDeclarationExprs;
 
     @Override
     public CompletableFuture<Object> executeCommand(ExecuteCommandParams params) {
@@ -41,29 +26,7 @@ public class MJGAWorkspaceService implements WorkspaceService {
                 String filePath = file.getPath().replaceAll("\"", "");
                 Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info("Parsing Java code from file: " + filePath);
 
-                try {
-                    ParserConfiguration parserConfig = new ParserConfiguration();
-                    parserConfig.setIgnoreAnnotationsWhenAttributingComments(true);
-                    parserConfig.setDoNotAssignCommentsPrecedingEmptyLines(true);
-                    StaticJavaParser.setConfiguration(parserConfig);
-                    compilationUnit = StaticJavaParser.parse(new FileInputStream(filePath));
-                    variableDeclarationExprs = compilationUnit.findAll(VariableDeclarator.class);
-                    compilationUnit.findAll(ForStmt.class)
-                        .forEach(forStmt -> {
-                            compilationUnit = new ForLoopRefactoringPattern().refactor(forStmt, compilationUnit);
-                        });
-
-                    compilationUnit.findAll(ForEachStmt.class)
-                        .forEach(forEachStmt -> {
-                            compilationUnit = new ForEachRefactoringPattern().refactor(forEachStmt, compilationUnit);
-                        });
-
-                    return compilationUnit.toString(new PrettyPrinterConfiguration().setOrderImports(true));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                return null;
+                return MJGALanguageServer.getInstance().getTextDocumentService().parseFile(filePath);
             }
 
             throw new UnsupportedOperationException();
@@ -79,9 +42,6 @@ public class MJGAWorkspaceService implements WorkspaceService {
     public void didChangeConfiguration(DidChangeConfigurationParams didChangeConfigurationParams) {
         this.configurationSettings = new JSONObject(didChangeConfigurationParams.getSettings().toString())
                 .getJSONObject("java");
-
-        Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).info( this.configurationSettings.toString());
-
     }
 
     @Override
