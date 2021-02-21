14767a97014842df425b8af3866660e3ccd5b1cd
Diogo Costa <hello@diogothecoder.com>
2021-02-21 22:44:47 +0000

    FYP-39: parse files in one place and format the path in one place

--- language-server/src/main/java/org/brunel/fyp/langserver/Utils.java
+++ language-server/src/main/java/org/brunel/fyp/langserver/Utils.java
@@ -1,12 +1,19 @@
 package org.brunel.fyp.langserver;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.logging.Level;
+
 public class Utils {
     public static final String SOURCE_NAME = "Make Java Great Again";
 
     public static String formatFileUri(String fileUri) {
-        fileUri = fileUri.replaceAll("\"", "");
-        fileUri = fileUri.replaceFirst("//", "");
-        fileUri = fileUri.replaceFirst("file:", "");
+        fileUri = fileUri.replaceFirst("file://", "");
+        fileUri = fileUri.replaceFirst("%3A", "");
+        fileUri = fileUri.replace("\\", "/");
+
+        MJGALanguageServer.LOGGER.info(fileUri);
 
         return fileUri;
     }
