37777ef3ed010bfab865bc638af1cf00b64479f6
Diogo Costa <hello@diogothecoder.com>
2021-02-22 16:50:50 +0000

    FYP-43: add test for running refactor command

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,6 +1,9 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import { activate, getDocPath, sleep } from './helper';
+import { Commands } from '../../commands';
+
+const hex = require('string-hex');
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
@@ -9,11 +12,7 @@ suite('Extension Test Suite', () => {
   test('Loading Java file', async () => {
     const textDocument = await vscode.workspace.openTextDocument(FILE_PATH);
 
-    assert.strictEqual(
-      textDocument.getText().length > 0,
-      true,
-      'Java file contains code',
-    );
+    assert.strictEqual(textDocument.getText().length > 0, true);
   });
 
   test('Connecting to Language Server', async () => {
@@ -41,5 +40,23 @@ suite('Extension Test Suite', () => {
     assert.strictEqual(mjgaDiagnostic.severity, 2);
     assert.strictEqual(mjgaDiagnostic.range.start.line, 5 - 1);
     assert.strictEqual(mjgaDiagnostic.range.end.line, 7 - 1);
+  });
+
+  test('Running Refactor Command', async () => {
+    const textDocument = await vscode.workspace.openTextDocument(FILE_PATH);
+
+    // Run the refactor command, wait for reply back, then save
+    await vscode.commands.executeCommand(Commands.REFACTOR_FILE);
+    await sleep(2000);
+    await textDocument.save();
+
+    const refactoredCode = textDocument.getText().trim();
+
+    // Simpler to compare hex values instead of the entire file
+    const refactoredCodeHex = hex(refactoredCode);
+    assert.strictEqual(
+      refactoredCodeHex,
+      '7075626c696320636c61737320417070207baa202020207075626c69632073746174696320766f6964206d61696e28537472696e675b5d206172677329207468726f777320457863657074696f6e207ba20202020202020204c6973743c537472696e673e206e616d6573203d204172726179732e61734c697374282244696f676f222c2022436f73746122293ba20202020202020206e616d65732e666f724561636828a202020202020202020202020737472696e67202d3e207ba2020202020202020202020202020202053797374656d2e6f75742e7072696e746c6e28737472696e67293ba2020202020202020202020207da2020202020202020293ba202020207da7d',
+    );
   });
 });
