872e91f8041e3b28c9ae2264e1831f07f39d932c
Diogo Costa <hello@diogothecoder.com>
2021-02-22 11:50:45 +0000

    FYP-42: create base test, just loading file

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,21 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import path from 'path';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Sample test', () => {
-    assert.equal([1, 2, 3].indexOf(5), -1);
-    assert.equal([1, 2, 3].indexOf(0), -1);
+  test('Loading Java file', async () => {
+    let filePath = path.join(__dirname, 'test/src/App.java');
+    filePath = filePath.replace('%3A', '');
+    const textDocument = await vscode.workspace.openTextDocument(filePath);
+
+    assert.strictEqual(
+      textDocument.getText().length > 0,
+      true,
+      'Java file contains code',
+    );
   });
 });
