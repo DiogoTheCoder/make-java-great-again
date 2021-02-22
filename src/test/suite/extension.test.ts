62ec0d5a6c4855facde5f96e77fdca550102742f
Diogo Costa <hello@diogothecoder.com>
2021-02-22 12:43:25 +0000

    FYP-42: created base extension test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,23 +1,34 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-import path from 'path';
-// import * as myExtension from '../../extension';
+import { activate, getDocPath } from './helper';
+import { Done } from 'mocha';
+import { strict, strictEqual } from 'assert';
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
+  const FILE_PATH = getDocPath('App.java');
   test('Loading Java file', async () => {
-    let filePath = path.join(__dirname, 'test/src/App.java');
-    filePath = filePath.replace('%3A', '');
-    const textDocument = await vscode.workspace.openTextDocument(filePath);
+    const textDocument = await vscode.workspace.openTextDocument(FILE_PATH);
 
     assert.strictEqual(
       textDocument.getText().length > 0,
       true,
       'Java file contains code',
     );
+  });
+
+  test('Connecting to Language Server', async () => {
+    // const ext = vscode.extensions.getExtension(
+    //   'DiogoTheCoder.make-java-great-again',
+    // )!;
+
+    await activate(FILE_PATH);
+
+    // assert.strictEqual(
+    //   ext.isActive,
+    //   true,
+    //   'Extension has been activated successfully',
+    // );
   });
 });
