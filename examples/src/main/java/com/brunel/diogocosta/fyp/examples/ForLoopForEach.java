2bbd11765b45f1347de4d3235a26b4fab5b83161
Diogo Costa <hello@diogothecoder.com>
2021-02-28 13:49:24 +0000

    FYP-49: revert changes to `ForLoopForEach`

--- examples/src/main/java/com/brunel/diogocosta/fyp/examples/ForLoopForEach.java
+++ examples/src/main/java/com/brunel/diogocosta/fyp/examples/ForLoopForEach.java
@@ -1,19 +1,12 @@
+  
 package com.brunel.diogocosta.fyp.examples;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-
 public class ForLoopForEach {
-
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Diogo", "Costa");
-        IntStream
-            .range(0, names.size())
-            .forEach(
-                i -> {
-                    System.out.println(names.get(i));
-                }
-            );
+        String[] names = { "Diogo", "Costa" };
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
