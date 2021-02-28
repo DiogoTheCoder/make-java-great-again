9ed0a8179d8d69d4e207bc801425516f3a93e17c
Diogo Costa <hello@diogothecoder.com>
2021-02-28 13:04:26 +0000

    FYP-49: check both `beforeRefactor` and `afterRefactor` are equal post-refactor

--- language-server/src/test/java/org/brunel/fyp/langserver/MJGAWorkspaceServiceTest.java
+++ language-server/src/test/java/org/brunel/fyp/langserver/MJGAWorkspaceServiceTest.java
@@ -1,19 +1,17 @@
 package org.brunel.fyp.langserver;
 
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import org.brunel.fyp.langserver.commands.Commands;
 import org.eclipse.lsp4j.ExecuteCommandParams;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.FileSystems;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,7 +39,16 @@ class MJGAWorkspaceServiceTest {
 
             // Let's now check if the refactor was done correctly
             CompilationUnit compilationUnit = StaticJavaParser.parse(completableFuture.get().toString());
-            String test = "";
+
+            // Should be of size two, beforeRefactor() and afterRefactor()
+            List<MethodDeclaration> methodDeclarations = compilationUnit.findAll(MethodDeclaration.class);
+            assertEquals(2, methodDeclarations.size());
+
+            MethodDeclaration beforeRefactorMethod = methodDeclarations.get(0);
+            MethodDeclaration afterRefactorMethod = methodDeclarations.get(1);
+
+            // Now let's check both before and after refactor methods have the same code!
+            assertEquals(afterRefactorMethod.getBody().get().toString(), beforeRefactorMethod.getBody().get().toString());
         } catch (Throwable e) {
             fail(e);
         }
