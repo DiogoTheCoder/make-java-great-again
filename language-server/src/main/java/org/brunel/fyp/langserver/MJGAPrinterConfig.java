9093a442977973f045226b686f86716885b93349
Diogo Costa <hello@diogothecoder.com>
2021-03-01 14:31:06 +0000

    FYP-52: upgraded JavaParser to `3.19.0`

---
+++ language-server/src/main/java/org/brunel/fyp/langserver/MJGAPrinterConfig.java
@@ -0,0 +1,22 @@
+package org.brunel.fyp.langserver;
+
+import com.github.javaparser.printer.DefaultPrettyPrinter;
+import com.github.javaparser.printer.configuration.DefaultConfigurationOption;
+import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration;
+import com.github.javaparser.printer.configuration.PrinterConfiguration;
+
+public final class MJGAPrinterConfig {
+    private static DefaultPrettyPrinter INSTANCE;
+
+    public static PrinterConfiguration getConfig() {
+        if (INSTANCE == null) {
+            INSTANCE = new DefaultPrettyPrinter();
+            INSTANCE.getConfiguration().addOption(new DefaultConfigurationOption(DefaultPrinterConfiguration.ConfigOption.ORDER_IMPORTS));
+            INSTANCE.getConfiguration().addOption(new DefaultConfigurationOption(DefaultPrinterConfiguration.ConfigOption.PRINT_COMMENTS));
+            INSTANCE.getConfiguration().addOption(new DefaultConfigurationOption(DefaultPrinterConfiguration.ConfigOption.PRINT_JAVADOC));
+            INSTANCE.getConfiguration().addOption(new DefaultConfigurationOption(DefaultPrinterConfiguration.ConfigOption.SPACE_AROUND_OPERATORS));
+        }
+
+        return INSTANCE.getConfiguration();
+    }
+}
