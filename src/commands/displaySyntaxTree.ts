b2b8a32fca2f90bd16701558385b775c7e0103ba
Diogo Costa <hello@diogothecoder.com>
2021-03-07 20:50:20 +0000

    FYP-2: updated graph to allow zooming

--- src/commands/displaySyntaxTree.ts
+++ src/commands/displaySyntaxTree.ts
@@ -39,16 +39,71 @@ function getWebviewContent(treeData: string) {
   <!DOCTYPE html>
   <meta charset="utf-8">
   <body>
-    <script src="https://d3js.org/d3.v6.min.js"></script>
+    <script src="https://d3js.org/d3.v5.min.js"></script>
     <script src="https://unpkg.com/@hpcc-js/wasm@0.3.11/dist/index.min.js"></script>
     <script src="https://unpkg.com/d3-graphviz@3.0.5/build/d3-graphviz.js"></script>
     <h1>Simple Abstract Syntax Tree</h1>
     <div id="graph" style="text-align: center;"></div>
-    <script>  
-    d3.select("#graph")
-      .graphviz()
-      .renderDot(\`${treeData}\`);
+    <script>
+      // to avoid scrollbars
+      var margin = 20;
+      
+      var graphviz = d3.select("#graph").graphviz()
+      .attributer(attributer)
+          .logEvents(true)
+          .on("initEnd", render);
+      
+          function attributer(datum, index, nodes) {
+          var selection = d3.select(this);
+          if (datum.tag == "svg") {
+              var width = window.innerWidth;
+              var height = window.innerHeight;
+              selection
+                  .attr("width", width)
+                  .attr("height", height)
+              datum.attributes.width = width - margin;
+              datum.attributes.height = height - margin;
+          }
+      }
+      
+      function render() {
+          graphviz
+          .renderDot(\`${treeData}\`)
+          .zoom(true);
+      }
+      
+      function attributer(datum, index, nodes) {
+          var selection = d3.select(this);
+          if (datum.tag == "svg") {
+              var width = window.innerWidth;
+              var height = window.innerHeight;
+              selection
+                  .attr("width", width)
+                  .attr("height", height)
+              datum.attributes.width = width - margin;
+              datum.attributes.height = height - margin;
+          }
+      }
+      
+      function resetZoom() {
+          graphviz
+              .resetZoom(d3.transition().duration(1000));
+      }
+      
+      function resizeSVG() {
+          console.log('Resize');
+          var width = window.innerWidth;
+          var height = window.innerHeight;
+          d3.select("#graph").selectWithoutDataPropagation("svg")
+              .transition()
+              .duration(700)
+              .attr("width", width - margin)
+              .attr("height", height - margin);
+      };
+      
+      d3.select(window).on("resize", resizeSVG);
+      d3.select(window).on("click", resetZoom);
     </script>
-  <body>
+    <body>
   `;
 }
